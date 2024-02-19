4e48075f0349a8c0911392b2b5be15b4eb93d95c
TerraInfinitia <160546487+TerraInfinitia@users.noreply.github.com>
2024-02-20 00:31:23 +0200

    lovely light

---
+++ jscripts/mainscript.js
@@ -0,0 +1,76 @@
+{
+	// Make span to serve as counter and mythology roulette span accessible in JavaScript.
+
+	let counter = document.getElementById("counter");
+	let mythologyroulette = document.getElementById("mythologyroulette");
+
+	
+	// Set date and enddate. enddate is already set in seconds, date is not. So, before being used, date is rounded and the result is then assigned to date.
+
+	let date = new Date() / 1000;
+	date = Math.round(date);
+	let enddate = new Date(1708390800);
+	let timeleft = enddate - date;
+
+
+	// Begin counter, keep it running every second (every 1000 miliseconds).
+	 
+	counter.innerHTML = timeleft + ' seconds';
+	function updateCounter () {
+		timeleft--;
+		counter.innerHTML = timeleft + ' seconds';
+	}
+
+	setInterval(updateCounter, 1000);
+
+
+	// Select random story for mythology roulette
+	// 0. "Ra"
+	// 1. "The Goddess Sekmet Comes To Life From Ra's Eye"
+	// 2. "Ra Thinks About Getting Some Rest"
+	// 3. "Nouit's Five Children"
+	// 4. "Osiris' Death"
+	// 5. "Osiris' Resurrection"
+	// 6. "Horus"
+	// 7. "The Chaos"
+	// 8. "Danae"
+	// 9. "Io"
+	// 10. "Leda"
+
+	let storynum = Math.floor(Math.random() * 11);
+	switch (storynum) {
+		case 0:
+			mythologyroulette.innerHTML = '<a href = "ra">"Ra"</a>';
+			break;
+		case 1:
+			mythologyroulette.innerHTML = '<a href = "sekmet">"The Goddess Sekmet Comes To Life From Ra\'s Eye"</a>';
+			break;
+		case 2:
+			mythologyroulette.innerHTML = '<a href = "rarests">"Ra Thinks About Getting Some Rest"</a>';
+			break;
+		case 3:
+			mythologyroulette.innerHTML = '<a href = "nouit">"Nouit\'s Five Children"</a>';
+			break;
+		case 4:
+			mythologyroulette.innerHTML = '<a href = "osirisdeath">"Osiris\' Death"</a>';
+			break;
+		case 5:
+			mythologyroulette.innerHTML = '<a href = "osirisresurrection">"Osiris\' Resurrection"</a>';
+			break;
+		case 6:
+			mythologyroulette.innerHTML = '<a href = "horus">"Horus"</a>';
+			break;
+		case 7:
+			mythologyroulette.innerHTML = '<a href = "thechaos">"The Chaos"</a>';
+			break;
+		case 8:
+			mythologyroulette.innerHTML = '<a href = "danae">"Danae"</a>';
+			break;
+		case 9:
+			mythologyroulette.innerHTML = '<a href = "io">"Io"</a>';
+			break;
+		case 10:
+			mythologyroulette.innerHTML = '<a href = "leda">"Leda"</a>';
+			break;
+	}
+}
